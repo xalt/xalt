448ed0cbe734da1f79cf6e652730c0c18323ade2
Robert McLay <mclay@zaniahgroup.com>
2014-04-24 19:59:30 -0500

    merged nic host name with tacc style

--- src/xalt_syshost.py
+++ src/xalt_syshost.py
@@ -1,17 +1,42 @@
 #!/usr/bin/env python
 # -*- python -*-
 from __future__ import print_function
-import platform
+import socket
 
-def syshost():
-  hostA = platform.node().split('.')
+def nics_syshost(fqdn):
+  nicsT = {
+    "kraken"    : "kraken",
+    "darter"    : "darter",
+    "kid"       : "kids",
+    "kfs"       : "keeneland",
+    "titan"     : "titan",
+    "nid00026"  : "mars",
+    "nid00027"  : "mars",
+    "verne"     : "verne",
+    "athena"    : "anthena",
+  }
+  result = None
+  for k in nicsT:
+    if (k.find(fqdn,0) != -1):
+      result = nicsT[k]
+      break
+  return result
+  
+def normal_syshost(fqdn):
+  hostA = fqdn.split('.')
   idx = 1 
   if (len(hostA) < 2):
     idx = 0
   return hostA[idx]
 
 def main():
-  print(syshost())
+
+  fqdn    = socket.getfqdn()
+  syshost = nics_syshost(fqdn)
+  if (not syshost):
+    syshost = normal_syshost(fqdn)
+
+  print(syshost)
   
 
 if ( __name__ == '__main__'): main()
