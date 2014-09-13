4c2d19559678ff02361f8bf6895fc83dc5885ef2
Robert McLay <mclay@zaniahgroup.com>
2014-09-13 11:37:21 -0500

    first test partially working

--- rt/common_funcs.sh
+++ rt/common_funcs.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 # -*- shell-script -*-
 
+initStdEnvVars()
+{
+  PATH=$outputDir/xalt/bin:$outputDir/xalt/sbin:$PATH;
+
+  ORIG_HOME=`(cd $HOME; /bin/pwd)`
+  HOME=`/bin/pwd`
+  numStep=0
+  COUNT=0
+}
+
+installXALT()
+{
+  rm -rf xalt
+  make -f $projectDir/makefile prefix=$outputDir/xalt PATH_TO_SRC=$projectDir \
+    install > /dev/null
+  cp $projectDir/src/removeDataBase.py xalt/sbin
+}
+
+installDB()
+{
+  DBNAME=testxalt
+  DBUSER=xaltTest
+  PASSWD=kutwgbh
+
+  conf_create.py     --dbhost localhost --dbuser $DBUSER \
+                     --passwd $PASSWD   --dbname $DBNAME
+  removeDataBase.py  --dbname $DBNAME  > /dev/null 2>&1
+  createDB.py        --dbname $DBNAME
+}
+
