9ae706e80039d473e79a161dd6f3e825565210a5
Robert McLay <mclay@zaniahgroup.com>
2014-11-03 16:16:01 -0600

    Convert db to use utf8 instead of latin1

--- src/createDB.in.py
+++ src/createDB.in.py
@@ -96,7 +96,7 @@ def main():
           `exec_path`     varchar(1024)  NOT NULL,
           PRIMARY KEY  (`link_id`),
           UNIQUE  KEY  `uuid` (`uuid`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=latin1  COLLATE=latin1_bin AUTO_INCREMENT=1
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8  COLLATE=utf8_general_ci AUTO_INCREMENT=1
         """)
     print("(%d) create xalt_link table" % idx); idx += 1
 
@@ -113,7 +113,7 @@ def main():
           PRIMARY KEY  (`obj_id`),
           INDEX  `index_hash_id` (`hash_id`),
           UNIQUE KEY `thekey` (`object_path`(512), `hash_id`, `syshost`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=latin1  COLLATE=latin1_bin AUTO_INCREMENT=1
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8  COLLATE=utf8_general_ci AUTO_INCREMENT=1
         """)
     print("(%d) create xalt_object table" % idx ); idx += 1;
 
@@ -127,7 +127,7 @@ def main():
           PRIMARY KEY (`join_id`),
           FOREIGN KEY (`link_id`) REFERENCES `xalt_link`(`link_id`),
           FOREIGN KEY (`obj_id`)  REFERENCES `xalt_object`(`obj_id`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=latin1  COLLATE=latin1_bin AUTO_INCREMENT=1
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8  COLLATE=utf8_general_ci AUTO_INCREMENT=1
         """)
     print("(%d) create join_link_object table" % idx); idx += 1
 
@@ -162,7 +162,7 @@ def main():
           PRIMARY KEY            (`run_id`),
           INDEX  `index_run_uuid` (`run_uuid`),
           INDEX `thekey` (`job_id`, `syshost`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin AUTO_INCREMENT=1
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=1
         """)
     print("(%d) create xalt_run table" % idx)
     idx += 1
@@ -177,7 +177,7 @@ def main():
           PRIMARY KEY (`join_id`),
           FOREIGN KEY (`run_id`)  REFERENCES `xalt_run`(`run_id`),
           FOREIGN KEY (`obj_id`)  REFERENCES `xalt_object`(`obj_id`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=latin1  COLLATE=latin1_bin AUTO_INCREMENT=1
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8  COLLATE=utf8_general_ci AUTO_INCREMENT=1
         """)
     print("(%d) create join_run_object table" % idx); idx += 1
 
@@ -189,7 +189,7 @@ def main():
           `env_name`      varchar(64)   NOT NULL,
           PRIMARY KEY  (`env_id`),
           UNIQUE  KEY  `env_name` (`env_name`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=latin1  COLLATE=latin1_bin AUTO_INCREMENT=1
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8  COLLATE=utf8_general_ci AUTO_INCREMENT=1
         """)
     print("(%d) create xalt_env_name table" % idx); idx += 1
 
@@ -203,7 +203,7 @@ def main():
           PRIMARY KEY (`join_id`),
           FOREIGN KEY (`env_id`)  REFERENCES `xalt_env_name`(`env_id`),
           FOREIGN KEY (`run_id`)  REFERENCES `xalt_run`(`run_id`) 
-        ) ENGINE=InnoDB DEFAULT CHARSET=latin1  COLLATE=latin1_bin AUTO_INCREMENT=1 
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8  COLLATE=utf8_general_ci AUTO_INCREMENT=1 
         """)
     print("(%d) create join_run_env table" % idx); idx += 1
 
@@ -213,7 +213,7 @@ def main():
           `inc_id`        int(11)        NOT NULL auto_increment,
           `job_id`        int(11)        NOT NULL,
           PRIMARY KEY (`inc_id`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=latin1  COLLATE=latin1_bin AUTO_INCREMENT=1
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8  COLLATE=utf8_general_ci AUTO_INCREMENT=1
         """)
     query = "INSERT INTO xalt_job_id VALUES(NULL, '0')"
     conn.query(query)
