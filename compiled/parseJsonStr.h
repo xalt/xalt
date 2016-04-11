0fd89fdc525b86d756c55bc37a302e7a160f3d63
Robert McLay <mclay@zaniahgroup.com>
2016-04-11 08:56:54 -0500

    changing to use processArray and processTable routine for buildRmapT.C

--- compiled/parseJsonStr.h
+++ compiled/parseJsonStr.h
@@ -2,9 +2,15 @@
 #define PARSEJSONSTR_H
 
 #include "xalt_types.h"
-void parseRunJsonStr(std::string& jsonStr, std::string& usr_cmdline, std::string& hash_id, Table& envT, Table& userT,
-                     Table& recordT, std::vector<Libpair>& libA);
-void parseLinkJsonStr(std::string& jsonStr, Vstring& linklineA, Table& resultT, std::vector<Libpair>& libA, Set& funcSet);
+void processArray(const char* name, const char* js, int& i, int ntokens, jsmntok_t* tokens, Vstring& vA);
+void processSet(const char* name, const char* js, int& i, int ntokens, jsmntok_t* tokens, Set& set);
+void processTable(const char* name, const char* js, int& i, int ntokens, jsmntok_t* tokens, Table& t);
+void processLibA(const char* name, const char* js, int& i, int ntokens, jsmntok_t* tokens, std::vector<Libpair>& libA);
+
+void parseRunJsonStr(const char* name, std::string& jsonStr, std::string& usr_cmdline, std::string& hash_id, Table& envT, 
+                     Table& userT, Table& recordT, std::vector<Libpair>& libA);
+void parseLinkJsonStr(const char* name, std::string& jsonStr, Vstring& linklineA, Table& resultT,
+                      std::vector<Libpair>& libA, Set& funcSet);
 
 
 
