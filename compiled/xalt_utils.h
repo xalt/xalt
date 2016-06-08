a1db5fd48e78ce1e964fcd80e08330eb8a230220
Robert McLay <mclay@zaniahgroup.com>
2016-06-07 21:10:34 -0500

    added rmapD to xalt_json_file_open

--- compiled/xalt_utils.h
+++ compiled/xalt_utils.h
@@ -6,7 +6,7 @@
 
 bool  path2module(const char* path, Table& rmapT, char* module_name, int module_name_sz);
 FILE* xalt_file_open(const char* name);
-FILE* xalt_json_file_open(const char* name);
+FILE* xalt_json_file_open(std::string rmapD, const char* name);
 int   isDirectory(const char *path);
 
 
