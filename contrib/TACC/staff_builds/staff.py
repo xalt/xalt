a96c7c1b5fab0f8ce1853e910e5ad28919bd8d2c
Robert McLay <mclay@zaniahgroup.com>
2016-06-11 16:33:52 -0500

    adding staff builds

---
+++ contrib/TACC/staff_builds/staff.py
@@ -0,0 +1,29 @@
+staffA = [
+  'agomez',
+  'alamas',
+  'bbarth',
+  'carlos',
+  'cazes',
+  'chenk',
+  'cprotor',
+  'djames',
+  'eijkhout',
+  'hliu',
+  'huang',
+  'karl',
+  'lwilson',
+  'mccalpin',
+  'mclay',
+  'milfeld'
+  'rauta',
+  'rtevans',
+  'siliu',
+  'viennej',
+  'vtrueheart',
+  'yye00'
+  ]
+
+
+def xalt_tacc_staff():
+  return staffA
+
