2ab0ed471df0c45e976907ddef5ebff62fa6e135
Robert McLay <mclay@zaniahgroup.com>
2016-06-11 08:25:31 -0500

    adding support for compiler usage by corehours

--- src/xalt_usage_report.py
+++ src/xalt_usage_report.py
@@ -161,6 +161,52 @@ class CompilerUsageByCount:
     
     return resultA
 
+class CompilerUsageByCoreHours:
+  def __init__(self, cursor):
+    self.__linkA  = []
+    self.__cursor = cursor
+
+  def build(self, args, startdate, enddate):
+    query = """
+    SELECT
+    ROUND(SUM(t1.run_time*t1.num_cores/3600)) as corehours,
+    COUNT(t1.date)                            as n_runs,
+    COUNT(DISTINCT(t1.user))                  as n_users,
+    t2.link_program                           as link_program
+    FROM xalt_run as t1, xalt_link as t2
+    WHERE t1.uuid is not NULL
+    AND   t1.uuid = t2.uuid
+    and   t1.syshost like %s
+    AND   t1.date >= %s and t1.date < %s
+    GROUP by link_program
+    """
+    cursor  = self.__cursor
+    cursor.execute(query, (args.syshost, startdate, enddate))
+    resultA = cursor.fetchall()
+    linkA   = self.__linkA
+    for link_program, count in resultA:
+      entryT = { 'corehours'   : corehours,
+                 'n_users'     : n_users,
+                 'n_runs'      : n_runs,
+                 'link_program': link_program
+               }
+      linkA.append(entryT)
+    
+
+  def report_by(self, args, sort_key):
+    resultA = []
+    resultA.append(['CoreHrs', '# Users','# Runs','Link Program'])
+    resultA.append(['-------', '-------','------','------------'])
+
+    linkA = self.__linkA
+    sortA = sorted(linkA, key=itemgetter(sort_key), reverse=True)
+    num = min(int(args.num), len(sortA))
+    for i in range(num):
+      entryT = sortA[i]
+      resultA.append(["%.0f" % (entryT['corehours']), "%d" % (entryT['n_users']), "%d" % (entryT['n_runs']), \
+                      entryT['link_program']])
+    return resultA
+
 class Libraries:
 
   def __init__(self, cursor):
@@ -527,7 +573,16 @@ def main():
   linkA.build(args, startdate, enddate)
   resultA = linkA.report_by(args, "count")
   bt      = BeautifulTbl(tbl=resultA, gap = 2, justify = "rl")
-  print("\nCompiler usage by count\n")
+  print("\nCompiler usage by Count\n")
+  print(bt.build_tbl())
+
+  ############################################################
+  # Report on Compiler (linker) usage by Core Hours
+  linkA = CompilerUsageByCoreHours(cursor)
+  linkA.build(args, startdate, enddate)
+  resultA = linkA.report_by(args, "corehours")
+  bt      = BeautifulTbl(tbl=resultA, gap = 2, justify = "rrrl")
+  print("\nCompiler usage by Corehours\n")
   print(bt.build_tbl())
 
   ############################################################
