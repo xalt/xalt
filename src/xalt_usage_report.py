2d9d7051ded8ef7696a027b38d01982ab9d93df9
Robert McLay <mclay@zaniahgroup.com>
2017-01-09 16:58:10 -0600

    adding reports for top codes by compiler

--- src/xalt_usage_report.py
+++ src/xalt_usage_report.py
@@ -70,8 +70,6 @@ class CmdLineOptions(object):
     args = parser.parse_args()
     return args
 
-
-
 class ExecRun:
   """ Holds the executables for a given date range """
   def __init__(self, cursor):
@@ -97,6 +95,63 @@ class ExecRun:
     sA.append("  GROUP BY execname ORDER BY totalcput DESC")
 
     query  = "".join(sA) % (args.syshost, startdate, enddate)
+    cursor = self.__cursor
+
+    cursor.execute(query)
+    resultA = cursor.fetchall()
+
+    execA = self.__execA
+    for execname, corehours, n_jobs, n_users in resultA:
+      entryT = {'execname'  : execname,
+                'corehours' : corehours,
+                'n_jobs'    : n_jobs,
+                'n_users'   : n_users}
+      execA.append(entryT)
+
+  def report_by(self, args, sort_key):
+    resultA = []
+    resultA.append(["CoreHrs", "# Jobs","# Users", "Exec"])
+    resultA.append(["-------", "------","-------", "----"])
+
+    execA = self.__execA
+    sortA = sorted(execA, key=itemgetter(sort_key), reverse=True)
+    num = min(int(args.num), len(sortA))
+    sumCH = 0.0
+    for i in range(num):
+      entryT = sortA[i]
+      resultA.append(["%.0f" % (entryT['corehours']),  "%d" % (entryT['n_jobs']) , "%d" %(entryT['n_users']), entryT['execname']])
+      sumCH += entryT['corehours']
+    
+    return resultA, sumCH
+
+
+class ExecRunLink:
+  """ Holds the executables for a given date range """
+  def __init__(self, cursor):
+    self.__execA  = []
+    self.__cursor = cursor
+
+  def build(self, args, startdate, enddate, compiler):
+    equiv_patternA = name_mapping()
+    sA = []
+    sA.append("SELECT CASE ")
+    for entry in equiv_patternA:
+      left  = entry[0].lower()
+      right = entry[1]
+      s     = "WHEN LOWER(SUBSTRING_INDEX(xalt_run.exec_path,'/',-1)) REGEXP '%s' then '%s' " % (left, right)
+      sA.append(s)
+
+    sA.append(" ELSE SUBSTRING_INDEX(t1.exec_path,'/',-1) END ")
+    sA.append(" AS execname, ROUND(SUM(t1.run_time*t1.num_cores/3600)) as totalcput, ")
+    sA.append(" COUNT(t1.date) as n_jobs, COUNT(DISTINCT(t1.user)) as n_users")
+    sA.append("   FROM xalt_run as t1, xalt_link as t2 ")
+    sA.append("  WHERE t1.syshost like '%s' ")
+    sA.append("    AND t1.date >= '%s' AND t1.date < '%s' ")
+    sA.append("    AND t1.uuid = t2.uuid")
+    sA.append("    AND t2.link_program = '%s' ")
+    sA.append("  GROUP BY execname ORDER BY totalcput DESC")
+
+    query  = "".join(sA) % (args.syshost, startdate, enddate, compiler)
     cursor = self.__cursor
 
     cursor.execute(query)
@@ -556,6 +611,26 @@ def main():
   print("\nTop",args.num, "MPI Executables sorted by # Users\n")
   print(bt.build_tbl())
   
+  ############################################################
+  #  Report of Top EXEC by Corehours for gcc
+  resultA, sumCH = execA.report_by(args,"corehours","gcc")
+  bt             = BeautifulTbl(tbl=resultA, gap = 2, justify = "rrrl")
+  print("\nTop",args.num, "MPI Executables sorted by Core-hours for gcc\n")
+  print(bt.build_tbl())
+
+  ############################################################
+  #  Report of Top EXEC by Corehours for g++
+  resultA, sumCH = execA.report_by(args,"corehours","g++")
+  bt             = BeautifulTbl(tbl=resultA, gap = 2, justify = "rrrl")
+  print("\nTop",args.num, "MPI Executables sorted by Core-hours for g++\n")
+  print(bt.build_tbl())
+  
+  ############################################################
+  #  Report of Top EXEC by Corehours for gfortran
+  resultA, sumCH = execA.report_by(args,"corehours","gfortran")
+  bt             = BeautifulTbl(tbl=resultA, gap = 2, justify = "rrrl")
+  print("\nTop",args.num, "MPI Executables sorted by Core-hours for gfortran\n")
+  print(bt.build_tbl())
 
   ############################################################
   #  Report of Top Modules by Core Hours
@@ -621,10 +696,4 @@ def main():
   print("")
   print(bt.build_tbl())
 
-
-  
-
-
 if ( __name__ == '__main__'): main()
-
-
