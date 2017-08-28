90eccc1f2a8352cb23354bd6a0a2183a57e6e0c7
Robert McLay <mclay@zaniahgroup.com>
2017-08-28 15:13:32 -0500

    bug fixes to get xalt_split_syslog_fn.py

--- src/xalt_split_syslog_fn.py
+++ src/xalt_split_syslog_fn.py
@@ -3,28 +3,26 @@
 from __future__ import print_function
 import os, sys, re
 
-class FILEH(object):
+class MY_FILEH(object):
   def __init__(self, fileName_pattern):
     self.__fileHandle = {}
     self.__pattern    = fileName_pattern
 
   def get_file_handle(self,syshost):
-    fileHandle = self.__fileHandle
-    if (not syshost in fileHandle):
+    if (not syshost in self.__fileHandle):
       fn = self.__pattern + "-" + syshost
-      fileHandle[syshost] = open(fn,"w")
-    return fileHandle[syshost]
+      self.__fileHandle[syshost] = open(fn,"w")
+    return self.__fileHandle[syshost]
 
   def close_file_handles(self):
     for fh in self.__fileHandle:
-      fh.close()
+      self.__fileHandle[fh].close()
 
 
 def main():
-  fileName = sys.argv[1]
-
-  fileH          = FILEH(fileName)
-  sysHostPattern = re.compile(r'syshost:(.*) ')
+  fileName       = sys.argv[1]
+  fileH          = MY_FILEH(fileName)
+  syshostPattern = re.compile(r'syshost:([^ ]*) ')
 
 
   with open(fileName) as f:
@@ -33,7 +31,7 @@ def main():
       if (not m):
         continue
       syshost = m.group(1)
-      fh = fileH(syshost)
+      fh = fileH.get_file_handle(syshost)
       fh.write(line)
 
   fileH.close_file_handles()
