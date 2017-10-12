9e120fc538cb789f5a9189b19f3ee7aa8a6926a0
Robert McLay <mclay@zaniahgroup.com>
2017-10-12 13:31:41 -0500

    adding comment to explain qcharA[]

--- compiled/xalt_quotestring.c
+++ compiled/xalt_quotestring.c
@@ -4,6 +4,9 @@
 #include <ctype.h>
 #include "xalt_quotestring.h"
 #define HERE fprintf(stderr,"%s:%d\n",__FILE__,__LINE__);fflush(stderr)
+
+// qchar maps ascii character value to their quoted version
+
 const char *qcharA[] = { "\\u0000","\\u0001","\\u0002","\\u0003","\\u0004","\\u0005","\\u0006","\\u0007",
                          "\\b"    ,"\\t"    ,"\\n"    ,"\\u000b","\\f"    ,"\\r"    ,"\\u000e","\\u000f",
                          "\\u0010","\\u0011",    "\\r","\\u0013","\\u0014","\\u0015","\\u0016","\\u0017",
