446cc8951b61233b1693867ac0fc28f27a825e15
Robert McLay <mclay@zaniahgroup.com>
2017-11-03 15:23:53 -0500

    testing compress and base64 code

--- compiled/compress_string.h
+++ compiled/compress_string.h
@@ -1,6 +1,14 @@
 #ifndef COMPRESS_STRING_H
 #define COMPRESS_STRING_H
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 int compress_string(const char* in, int compressionlevel, char* out, int* lenOut);
 
+#ifdef __cplusplus
+}
+#endif
 #endif //COMPRESS_STRING_H
