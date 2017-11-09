d3914cbff86a9ca2cfe202dd4dbfdbf7073d496d
Robert McLay <mclay@zaniahgroup.com>
2017-11-09 11:15:05 -0600

    add pretend R data to test_record_pkg.C

--- compiled/test_record_pkg.C
+++ compiled/test_record_pkg.C
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
 {
   if (argc != 2)
     {
-      fprintf(stderr,"Need the full path to xalt_record_pkg => quiting!\n");
+      fprintf(stderr,"Need the full path to the directory for xalt_record_pkg => quiting!\n");
       exit(1);
     }
 
@@ -15,19 +15,34 @@ int main(int argc, char* argv[])
   if (! run_uuid)
     exit(0);
 
-  char* record_prgm = argv[1];
+  char* xaltDir = argv[1];
 
   std::string cmd;
   std::string syshost;
 
-  cmd.assign();
-  cmd.append(" ");
-  
+  cmd.assign(xaltDir);
+  cmd.append("/libexec/xalt_syshost");
+
+  Vstring v;
     
     
 
-  cap
+  capture(cmd,v);
+  std::string syshost = v[0];
 
+  cmd.assign(xaltDir);
+  cmd.append("/libexec/xalt_record_pkg -s ");
+  cmd.append(syshost);
+  cmd.append(" -u ");
+  cmd.append(run_uuid);
+  cmd.append(" program R");
+  cmd.append(" xalt_run_uuid ");
+  cmd.append(run_uuid);
+  cmd.append(" package_name acme");
+  cmd.append(" package_verion 1.0");
+  cmd.append(" package_file /A/B/acme/1.0");
   
+  printf("%s\n",cmd.c_str());
+    
   return 0;
 }
