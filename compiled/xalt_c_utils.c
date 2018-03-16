7638ea253b829124751ca20794a2ca4189890214
Robert McLay <mclay@zaniahgroup.com>
2018-03-16 16:29:50 -0500

    add mkpath() function to support recursive mkdir

--- compiled/xalt_c_utils.c
+++ compiled/xalt_c_utils.c
@@ -1,6 +1,8 @@
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include "xalt_c_utils.h"
 
 int isDirectory(const char *path)
 {
@@ -8,4 +10,23 @@ int isDirectory(const char *path)
   if (stat(path, &statbuf) != 0)
     return 0;
   return S_ISDIR(statbuf.st_mode);
+}
+
+int mkpath(char* file_path, mode_t mode)
+{
+  char* p;
+  for (p=strchr(file_path+1, '/'); p; p=strchr(p+1, '/'))
+    {
+      *p='\0';
+      if (mkdir(file_path, mode)==-1)
+	{
+	  if (errno != EEXIST)
+	    {
+	      *p='/';
+	      return -1;
+	    }
+	}
+      *p='/';
+    }
+  return 0;
 }
--- compiled/xalt_c_utils.h
+++ compiled/xalt_c_utils.h
@@ -5,7 +5,11 @@ extern "C"
 {
 #endif
 
+#include <sys/types.h>
+#include <sys/stat.h>
+
 int   isDirectory(const char *path);
+int   mkpath(char *path, mode_t mode);
 
 #ifdef __cplusplus
 }
