8b59b7a2fe01505e24ea9352c2a4e95b34438f0a
Robert McLay <mclay@zaniahgroup.com>
2019-03-08 16:48:35 -0600

    starting to work on generating c code for watermark

--- src/xalt_generate_assembly.C
+++ src/xalt_generate_assembly.C
@@ -13,6 +13,7 @@ int main(int argc, char* argv[])
   const char * uuid    = argv[i++];
   const char * syshost = argv[i++];
   const char * fn      = argv[i++];
+  const char * fn_c    = argv[i++];
   std::string  jsonStr = argv[i++];
   const char * version = XALT_VERSION;
 
@@ -65,6 +66,25 @@ int main(int argc, char* argv[])
   std::string system = u.sysname;
   std::string host   = u.nodename;
 
+  std::string array[13][2];
+
+  int k = -1;
+  ++k; array[k][0] = "<XALT_Version>";        array[k][1] = version;          /*  0 */
+  ++k; array[k][0] = "<Build_host>";          array[k][1] = host;             /*  1 */
+  ++k; array[k][0] = "<Build_Syshost>";       array[k][1] = syshost;          /*  2 */
+  ++k; array[k][0] = "<Build_compiler>";      array[k][1] = compiler;         /*  3 */
+  ++k; array[k][0] = "<Build_compilerPath>";  array[k][1] = compilerPath;     /*  4 */
+  ++k; array[k][0] = "<Build_OS>";            array[k][1] = osName;           /*  5 */
+  ++k; array[k][0] = "<Build_User>";          array[k][1] = user;             /*  6 */
+  ++k; array[k][0] = "<Build_UUID>";          array[k][1] = uuid;             /*  7 */
+  ++k; array[k][0] = "<Build_Year>";          array[k][1] = year;             /*  8 */
+  ++k; array[k][0] = "<Build_date>";          array[k][1] = date;             /*  9 */
+  ++k; array[k][0] = "<Build_LOADEDMODULES>"; array[k][1] = loadedmodules;    /* 10 */
+  ++k; array[k][0] = "<Build_LMFILES>";       array[k][1] = lmfiles;          /* 11 */
+  ++k; array[k][0] = "<Build_Epoch>";         array[k][1] = epochStr;         /* 12 */
+
+  int n = k+1;
+
   FILE* fp = fopen(fn,"w");
 
   //--------------------------------------------------
@@ -104,25 +124,62 @@ int main(int argc, char* argv[])
 
   // Print cushion
   fprintf(fp,"\n\t.byte 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00\n");
-  fprintf(fp,"\t.asciz \"<XALT_Version>%%%%%s%%%%\"\n",version);
-  fprintf(fp,"\t.asciz \"<Build_host>%%%%%s%%%%\"\n",host.c_str());
-  fprintf(fp,"\t.asciz \"<Build_Syshost>%%%%%s%%%%\"\n",syshost);
-  fprintf(fp,"\t.asciz \"<Build_compiler>%%%%%s%%%%\"\n",compiler.c_str());
-  fprintf(fp,"\t.asciz \"<Build_compilerPath>%%%%%s%%%%\"\n",compilerPath.c_str());
-  fprintf(fp,"\t.asciz \"<Build_OS>%%%%%s%%%%\"\n",osName.c_str());
-  fprintf(fp,"\t.asciz \"<Build_User>%%%%%s%%%%\"\n",user);
-  fprintf(fp,"\t.asciz \"<Build_UUID>%%%%%s%%%%\"\n",uuid);
-  fprintf(fp,"\t.asciz \"<Build_Year>%%%%%s%%%%\"\n",year);
-  fprintf(fp,"\t.asciz \"<Build_date>%%%%%s%%%%\"\n",date);
-  fprintf(fp,"\t.asciz \"<Build_Epoch>%%%%%s%%%%\"\n",epochStr);
-  fprintf(fp,"\t.asciz \"<Build_LOADEDMODULES>%%%%%s%%%%\"\n",loadedmodules);
-  fprintf(fp,"\t.asciz \"<Build_LMFILES>%%%%%s%%%%\"\n",lmfiles);
-  fprintf(fp,"\t.asciz \"<Build_Epoch>%%%%%s%%%%\"\n",epochStr);
+  for (int j = 0; j < n; ++j)
+    fprintf(fp,"\t.asciz \"%s%%%%%s%%%%\"\n",array[j][0].c_str(), array[j][1].c_str());
+
   fprintf(fp,"\t.byte 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00\n");
   fprintf(fp,"\t.asciz \"XALT_Link_Info_End\"\n");
 
   fclose(fp);
   
+  fp = fopen(fn_c,"w");
+
+  //------------------------------------------------------
+  // Write c version
+
+  fprintf(fp,"/* This generated assembly code (%s) is free and unencumbered software released into the public domain */\n",fn_c);
+  fprintf(fp,"/* Anyone is free to copy, modify, publish, use, compile, sell, or */\n");
+  fprintf(fp,"/* distribute this software, either in source code form or as a compiled */\n");
+  fprintf(fp,"/* binary, for any purpose, commercial or non-commercial, and by any */\n");
+  fprintf(fp,"/* means.*/\n");
+  fprintf(fp,"\n");
+  fprintf(fp,"/* In jurisdictions that recognize copyright laws, the author or authors */\n");
+  fprintf(fp,"/* of this software dedicate any and all copyright interest in the */\n");
+  fprintf(fp,"/* software to the public domain. We make this dedication for the benefit */\n");
+  fprintf(fp,"/* of the public at large and to the detriment of our heirs and */\n");
+  fprintf(fp,"/* successors. We intend this dedication to be an overt act of */\n");
+  fprintf(fp,"/* relinquishment in perpetuity of all present and future rights to this */\n");
+  fprintf(fp,"/* software under copyright law. */\n");
+  fprintf(fp,"\n");
+  fprintf(fp,"/* THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, */\n");
+  fprintf(fp,"/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF */\n");
+  fprintf(fp,"/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */\n");
+  fprintf(fp,"/* IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR */\n");
+  fprintf(fp,"/* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE */,\n");
+  fprintf(fp,"/* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR */\n");
+  fprintf(fp,"/* OTHER DEALINGS IN THE SOFTWARE.*/\n");
+  fprintf(fp,"\n\n");
+  fprintf(fp,"#include <stdio.h>\n");
+  fprintf(fp,"#include \"xalt_obfuscate.h\"\n");
+  fprintf(fp,"char * xalt_db()\n");
+  fprintf(fp,"{\n");
+  fprintf(fp,"  char * xalt_str = NULL;\n");
+  fprintf(fp,"  asprintf(&xalt_str,\n");
+
+  for (int j = 0; j < n; ++j)
+    fprintf(fp,"                   \"%%s%%%%%%%%%%s%%%%%%%% \"\n");
+  fprintf(fp,"                     ,\n");
+
+  for (int j = 0; j < n-1; ++j)
+    fprintf(fp,"                   \"%s\",\"%s\",\n",  array[j][0].c_str(),  array[j][1].c_str());
+  fprintf(fp,"                     \"%s\",\"%s\");\n", array[n-1][0].c_str(),array[n-1][1].c_str());
+  fprintf(fp,"  return xalt_str;\n");
+  fprintf(fp,"}\n");
+  
+  fclose(fp);
+
+
+
   fprintf(stdout,"%s\n",epochStr);
   free(epochStr);
   return 0;
