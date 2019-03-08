62174ce18caee0fee495838aa3ba410bf90d845f
Robert McLay <mclay@zaniahgroup.com>
2019-03-08 10:40:04 -0600

    xalt_obfuscate.h does not need to be in the extern C++ foo in xalt_tmpdir.h

--- src/xalt_tmpdir.h
+++ src/xalt_tmpdir.h
@@ -1,12 +1,13 @@
 #ifndef XALT_TMPDIR_H
 #define XALT_TMPDIR_H
 
+#include "xalt_obfuscate.h"
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-#include "xalt_obfuscate.h"
 
 char* create_xalt_tmpdir_str(const char* run_uuid);
 void  remove_xalt_tmpdir(    const char* run_uuid);
