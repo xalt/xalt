e5abd64f78c0b5e27b0310d857579d7d1025be5d
Robert McLay <mclay@zaniahgroup.com>
2019-12-06 19:26:32 -0600

    return 0 when locate_shared_library.c is successful

--- proj_mgmt/locate_shared_library.c
+++ proj_mgmt/locate_shared_library.c
@@ -24,5 +24,6 @@ int main(int argc, char** argv)
   int result = dlinfo(handle, RTLD_DI_LINKMAP,  &map);
   char * name = realpath(map->l_name, NULL);
   printf("%s:%s\n",map->l_name, name);
+  return 0;
 }
   
