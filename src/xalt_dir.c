01db66e949ffcfbafee7a4b7227187ab3e7faddb
Robert McLay <mclay@zaniahgroup.com>
2020-03-07 20:43:35 -0600

    check $XALT_DIR and then XALT_DEFAULT_DIR when trying to find xalt helper files

--- src/xalt_dir.c
+++ src/xalt_dir.c
@@ -4,28 +4,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <syslog.h>
 #include <unistd.h>
 #include "xalt_config.h"
 
-const char* xalt_dir(const char* file) {
-  const char* dir = getenv("XALT_DIR") ? getenv("XALT_DIR") : XALT_DEFAULT_DIR;
+const char* xalt_dir(const char* file)
+{
+  int i;
+  char *joined;
+  const char** dirA[2];
 
-  if (!file) {
-    return dir;
-  }
-  else {
-    char *joined;
-    int ret = asprintf(&joined, "%s/%s", dir, file);
-    if (ret > 0) {
-      if (access(joined, F_OK) != 0) 
-        syslog(LOG_WARNING, "XALT unable to access '%s'", joined);
+  dirA[0] = getenv("XALT_DIR");
+  dirA[1] = XALT_DEFAULT_DIR;
 
-      /* this will leak memory if the caller does not free() the string */
-      return joined;
+  for (i = 0; i < 2; ++i)
+    {
+      const char* dir = dirA[i];
+      if (dir == NULL)
+        continue;
+
+      if (!file) 
+        return dir;
+
+      int ret = asprintf(&joined, "%s/%s", dir, file);
+      if (ret > 0 && access(joined, R_OK) != -1)
+        {
+          /* this will leak memory if the caller does not free() the string */
+          return joined;
+        }
     }
-    else {
-      return file;
-    }
-  }
+  return file;
 }
