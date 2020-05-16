23e40e74bbc868768cf3a5f4c684dbb0c07581b6
Robert McLay <mclay@zaniahgroup.com>
2020-05-16 15:34:58 -0500

    fullDateStr needs to be twice the size of dateStr

--- src/xalt_initialize.c
+++ src/xalt_initialize.c
@@ -84,6 +84,7 @@
 
 
 #define DATESZ    100
+#define DATESZ2   200
 
 typedef enum { BIT_SCALAR = 1, BIT_PKGS = 2, BIT_MPI = 4} xalt_tracking_flags;
 typedef enum { PKGS=1, KEEP=2, SKIP=3} xalt_parser;
@@ -245,7 +246,7 @@ void myinit(int argc, char **argv)
   char   rand_str[20];
   char * pid_str         = NULL;
   char   dateStr[DATESZ];
-  char   fullDateStr[DATESZ];
+  char   fullDateStr[DATESZ2];
   const char * v;
   xalt_parser  results;
   xalt_parser  path_results;
