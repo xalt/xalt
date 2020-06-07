d86f4ed5d8575887851ee370e5d411733e7b86fc
Robert McLay <mclay@zaniahgroup.com>
2020-06-07 17:14:52 -0500

    Setup to test python package recording

---
+++ py_src/xalt_sitecustomize.py
@@ -0,0 +1,101 @@
+#### this should go into sitecustomize.py ####
+import sys, os, subprocess
+
+from xalt_pkg_filter import keep_pkg
+
+class RecorderRTM(object):
+  """
+  Record all requests to load a module by name.
+  """
+  __slots__ = ['_cmd','_keepT']
+
+
+  def __init__(self, uuid, version_str):
+    self._keepT   = {}
+    self._cmd     = "XALT_EXECUTABLE_TRACKING=no " + os.path.join(os.environ.get("XALT_DIR","/unknown"),"libexec/xalt_report_pkg") + 
+                    " -u " + uuid + " program python xalt_run_uuid " + uuid + " package_version " + version_str
+    
+    
+  def __keep(self, fullname, path):
+    keepT              = self._keepT
+    keep, reason, kind = keep_pkg(fullname, path)
+    if (not keep):
+      return False
+
+    if (fullname in keepT):
+      return False
+    keepT[fullname] = True
+    return True
+
+  def __report(self, fullname, path):
+    cmd = self._cmd + " package_name " + fullname + " package_path " + path
+    subprocess.call(cmd, shell=True)
+
+
+  # Python 3.4+
+  def find_spec(self, fullname, path, target=None):
+
+    result = None
+
+    for entry in sys.meta_path:
+      cls  = type(entry)
+      if (cls.__name__ != "RecorderRTM" ):
+        try: 
+          result = entry.find_spec(fullname, path, target)
+        except:
+          pass
+        if (result):
+          break
+
+    if (not result):
+      return result
+
+    path = result.origin
+
+    if (self.__keep(fullname, path)):
+      self._report(fullname, path)
+
+    return result
+
+  # Python 2.3--3.3
+  def find_module(self, fullname, path):
+    result = None
+    path   = None
+
+    for entry in sys.meta_path:
+      cls  = type(entry)
+      if (cls.__name__ != "RecorderRTM" ):
+        result = entry.find_module(fullname, path)
+        if (result):
+          path = result.origin
+          break
+  
+    for entry in sys.path:
+      base = os.path.join(entry,fullname)
+      fn   = base + ".py"
+      if (os.path.exists(fn)):
+        path = fn
+        break
+      else:
+        fn   = os.path.join(base, "__init__.py")
+        if (os.path.exists(fn)):
+          path = fn
+          break
+
+    if (path and  self.__keep(fullname, path)):
+      self._report(fullname, path)
+
+    return result
+
+
+uuid        = os.environ.get("XALT_RUN_UUID",None)
+if (uuid):
+  sA = []
+  for i in sys.version_info:
+    s = str(i)
+    if (s == 'final'):
+      break
+  version_str = '.'.join(sA)
+  sA.append(s)
+  sys.meta_path.insert(0, RecorderRTM(uuid, version_str))
+
