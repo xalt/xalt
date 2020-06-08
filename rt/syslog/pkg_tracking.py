5701c7abf4150529a1015fcb82b35131727c75c3
Robert McLay <mclay@zaniahgroup.com>
2020-06-08 15:51:30 -0500

    add py test code to syslog

---
+++ rt/syslog/pkg_tracking.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+import os, sys, re, json, time, base64, traceback
+
+def main():
+  print ("Hello World! from ", sys.version)
+
+
+if ( __name__ == '__main__'): main()
