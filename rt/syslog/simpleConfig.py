5eabea682317fe53d89fcf1b2ec98a1f44de51d3
Robert McLay <mclay@zaniahgroup.com>
2020-06-08 16:42:22 -0500

    Add in python patterns for py pkg test

--- rt/syslog/simpleConfig.py
+++ rt/syslog/simpleConfig.py
@@ -3,7 +3,8 @@ hostname_patterns = [
   ['KEEP', '.*']
 ]
 path_patterns = [
-  ['PKGS',  r'.*\/test_record_pkg'],
+  ['PKGS', r'.*\/test_record_pkg'],
+  ['PKGS', r'.*\/python[0-9][^/][^/]*'],
   ['SKIP', r'.*\/lua'],
   ['SKIP', r'.*\/expr'],
   ['SKIP', r'.*\/cc1'],
@@ -11,4 +12,12 @@ path_patterns = [
   ['SKIP', r'.*\/collect2'],
   ['SKIP', r'.*\/mpich/.*'],
   ['SKIP', r'.*\/x86_64-linux-gnu.*'],
+]
+
+python_pkg_patterns = [
+  { 'k_s' : 'SKIP', 'kind' : 'path', 'patt' : r"^[^/]"      },  # SKIP all built-in packages
+  { 'k_s' : 'SKIP', 'kind' : 'name', 'patt' : r"^_"         },  # SKIP names that start with a underscore
+  { 'k_s' : 'SKIP', 'kind' : 'name', 'patt' : r".*\."       },  # SKIP all names that are divided with periods: a.b.c
+  { 'k_s' : 'KEEP', 'kind' : 'path', 'patt' : r".*/.local/" },  # KEEP all packages installed by users
+  { 'k_s' : 'SKIP', 'kind' : 'path', 'patt' : r"/home"      },  # SKIP all other packages in user locations
 ]
