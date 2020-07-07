68ae00e4c0589992b86e51b951627a29cbf52ba1
Robert McLay <mclay@zaniahgroup.com>
2020-07-07 17:39:53 -0500

    c based resultDir() added

--- src/util/xalt_c_utils.c
+++ src/util/xalt_c_utils.c
@@ -30,3 +30,30 @@ int mkpath(char* file_path, mode_t mode)
     }
   return 0;
 }
+
+void build_resultDir(char **resultDir, const char* kind, const char* transmission, const char* uuid_str)
+{
+
+  char* c_home = getenv("HOME");
+  utstring_new(dir);
+  
+  const char * xalt_file_prefix = getenv("XALT_FILE_PREFIX");
+  if (xalt_file_prefix == NULL)
+    xalt_file_prefix = XALT_FILE_PREFIX;
+
+  if ((c_home != NULL) && (strcasecmp(xalt_file_prefix,"USE_HOME") == 0))
+    {
+      if (strcasecmp(transmission,"file_separate_dirs") == 0)
+	asprintf(&resultDir,"%s/.xalt.d/%s/",c_home,kind);
+      else
+	asprintf(&resultDir,"%s/.xalt.d/",c_home);
+    }
+  else
+    {
+      long hashV = strtol(&uuid[29], 0, 16) % XALT_PRIME_NUMBER;
+      if (strcasecmp(transmission,"file_separate_dirs") == 0)
+	asprintf(&resultDir, "%s/%s/" XALT_PRIME_FMT, "/",xalt_file_prefix,kind,hashV);
+      else
+	asprintf(&resultDir, "%s/" XALT_PRIME_FMT, "/",xalt_file_prefix,hashV);
+    }
+}
