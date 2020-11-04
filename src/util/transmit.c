b73cbfbf7723edac8f04e1dfe46aa21a02b7ed8e
Robert McLay <mclay@zaniahgroup.com>
2020-11-04 14:36:21 -0600

    Issue #38: transport raw jsonStr instead of compress/base64 encoding

--- src/util/transmit.c
+++ src/util/transmit.c
@@ -81,18 +81,97 @@ void transmit(const char* transmission, const char* jsonStr, const char* kind, c
       my_free(tmpFn,strlen(tmpFn));
       my_free(fn,   strlen(fn));
     }
+  else if (strcasecmp(transmission, "syslog") == 0)
+    {
+      int   sz      = strlen(jsonStr);
+      int   blkSz   = (sz < syslog_msg_sz) ? sz : syslog_msg_sz;
+      int   nBlks   = (sz -  1)/blkSz + 1;
+      int   istrt   = 0;
+      int   iend    = blkSz;
+      int   i;
+
+      asprintf(&logNm, "XALT_LOGGING_%s",syshost);
+      openlog(logNm, 0, LOG_USER);
+
+      for (i = 0; i < nBlks; i++)
+        {
+          syslog(LOG_INFO, "V:3 kind:%s idx:%d nb:%d syshost:%s key:%s value:%.*s",
+                   kind, i, nBlks, syshost, key, iend-istrt, &jsonStr[istrt]);
+          istrt = iend;
+          iend  = istrt + blkSz;
+          if (iend > sz)
+            iend = sz;
+        }
+      closelog();
+      my_free(logNm, strlen(logNm));
+    }
+  else if (strcasecmp(transmission, "curl") == 0)
+    {
+      int pid, status, ret = 0;
+      char *myargs [] 	= { NULL, NULL, NULL, NULL};
+      char *myenv  [] 	= { NULL };
+
+      // Prepend $LIB64 to $LD_LIBRARY_PATH
+      int  i;
+      char *ld_lib_path = getenv("LD_LIBRARY_PATH");
+      char *lib64_dir   = xalt_dir("lib64");
+      int  len_lpath    = strlen(ld_lib_path);
+      int  len_l64dir   = strlen(lib64_dir);
+      int  len          = len_l64dir + len_lpath + 2;
+      char *value       = (char *) malloc(len*sizeof(char));
+
+      i = 0;
+      memcpy(&value[i], lib64_dir,   len_l64dir); i += len_l64dir;
+      value[i] = ':';                             i += 1;
+      memcpy(&value[i], ld_lib_path, len_lpath);  i += len_lpath;
+      value[i] = '\0';                            i += 1;
+      setenv("LD_LIBRARY_PATH", value, 1);
+      my_free(value,len);
+
+      // Define arguments to xalt_curl_transmit.c
+      char* prgm = xalt_dir("libexec/xalt_curl_transmit"); 
+      myargs[0]  = prgm;
+      myargs[1]  = (char *) syshost;
+      myargs[2]  = (char *) jsonStr;
+
+      // Call xalt_curl_transmit as a child process and wait for it to complete.
+      pid = fork();
+      if (pid == 0)
+	{
+	  // Child process
+	  execve(prgm, myargs, myenv);
+	}
+
+      // Parent: Wait for child to complete
+      if ((ret = waitpid (pid, &status, 0)) != 0)
+	{
+	  DEBUG0(my_stderr, "  waitpid() returned -1: error with xalt_curl_transmit\n");
+	  asprintf(&logNm, "XALT_LOGGING_ERROR_%s",syshost);
+	  openlog(logNm, LOG_PID, LOG_USER);
+	  syslog(LOG_INFO, "waitpid() returned -1: error with xalt_curl_transmit");
+	  closelog();
+	  my_free(logNm,strlen(logNm));
+	  return;
+	}
+
+      // Free memory
+      my_free(prgm, strlen(prgm));
+    }
   else if (strcasecmp(transmission, "syslogv1") == 0)
     {
-      int   zslen;
-      int   b64len;
-      char* zs      = compress_string(jsonStr,&zslen);
-      char* b64     = base64_encode(zs, zslen, &b64len);
+      int   sz      = strlen(jsonStr);
+      int   blkSz   = (sz < syslog_msg_sz) ? sz : syslog_msg_sz;
+      int   nBlks   = (sz -  1)/blkSz + 1;
+      int   istrt   = 0;
+      int   iend    = blkSz;
+      int   i;
 
-      asprintf(&cmdline, "LD_PRELOAD= XALT_EXECUTABLE_TRACKING=no PATH=%s logger -t XALT_LOGGING_%s \"%s:%s\"\n",XALT_SYSTEM_PATH, syshost, kind, b64);
-      system(cmdline);
-      my_free(zs,zslen);
-      my_free(b64,b64len);
-      my_free(cmdline,strlen(cmdline));
+      asprintf(&logNm, "XALT_LOGGING_%s",syshost);
+      openlog(logNm, 0, LOG_USER);
+
+      syslog(LOG_INFO, "%s:%s", kind, &jsonStr[0])
+      closelog();
+      my_free(logNm, strlen(logNm));
     }
   else if (strcasecmp(transmission, "logger") == 0)
     {
@@ -171,88 +250,5 @@ void transmit(const char* transmission, const char* jsonStr, const char* kind, c
       my_free(syshostStr, strlen(syshostStr));
       my_free(keyStr,     strlen(keyStr));
       my_free(valueStr,   nvalueStr);
-    }
-  else if (strcasecmp(transmission, "syslog") == 0)
-    {
-      int   sz;
-      int   zslen;
-      char* zs      = compress_string(jsonStr, &zslen);
-      char* b64     = base64_encode(zs, zslen, &sz);
-      
-      
-      int   blkSz   = (sz < syslog_msg_sz) ? sz : syslog_msg_sz;
-      int   nBlks   = (sz -  1)/blkSz + 1;
-      int   istrt   = 0;
-      int   iend    = blkSz;
-      int   i;
-
-      asprintf(&logNm, "XALT_LOGGING_%s",syshost);
-      openlog(logNm, 0, LOG_USER);
-
-      for (i = 0; i < nBlks; i++)
-        {
-          syslog(LOG_INFO, "V:2 kind:%s idx:%d nb:%d syshost:%s key:%s value:%.*s",
-                   kind, i, nBlks, syshost, key, iend-istrt, &b64[istrt]);
-          istrt = iend;
-          iend  = istrt + blkSz;
-          if (iend > sz)
-            iend = sz;
-        }
-      closelog();
-      my_free(b64,   sz);
-      my_free(zs,    zslen);
-      my_free(logNm, strlen(logNm));
-    }
-  else if (strcasecmp(transmission, "curl") == 0)
-    {
-      int pid, status, ret = 0;
-      char *myargs [] 	= { NULL, NULL, NULL, NULL};
-      char *myenv  [] 	= { NULL };
-
-      // Prepend $LIB64 to $LD_LIBRARY_PATH
-      int  i;
-      char *ld_lib_path = getenv("LD_LIBRARY_PATH");
-      char *lib64_dir   = xalt_dir("lib64");
-      int  len_lpath    = strlen(ld_lib_path);
-      int  len_l64dir   = strlen(lib64_dir);
-      int  len          = len_l64dir + len_lpath + 2;
-      char *value       = (char *) malloc(len*sizeof(char));
-
-      i = 0;
-      memcpy(&value[i], lib64_dir,   len_l64dir); i += len_l64dir;
-      value[i] = ':';                             i += 1;
-      memcpy(&value[i], ld_lib_path, len_lpath);  i += len_lpath;
-      value[i] = '\0';                            i += 1;
-      setenv("LD_LIBRARY_PATH", value, 1);
-      my_free(value,len);
-
-      // Define arguments to xalt_curl_transmit.c
-      char* prgm = xalt_dir("libexec/xalt_curl_transmit"); 
-      myargs[0]  = prgm;
-      myargs[1]  = (char *) syshost;
-      myargs[2]  = (char *) jsonStr;
-
-      // Call xalt_curl_transmit as a child process and wait for it to complete.
-      pid = fork();
-      if (pid == 0)
-	{
-	  // Child process
-	  execve(prgm, myargs, myenv);
-	}
-
-      // Parent: Wait for child to complete
-      if ((ret = waitpid (pid, &status, 0)) != 0)
-	{
-	  DEBUG0(my_stderr, "  waitpid() returned -1: error with xalt_curl_transmit\n");
-	  asprintf(&logNm, "XALT_LOGGING_ERROR_%s",syshost);
-	  openlog(logNm, LOG_PID, LOG_USER);
-	  syslog(LOG_INFO, "waitpid() returned -1: error with xalt_curl_transmit");
-	  closelog();
-	  my_free(logNm,strlen(logNm));
-	  return;
-	}
-
-      // Free memory
-      my_free(prgm, strlen(prgm));
     }
 }
