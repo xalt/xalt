7ae62e994b23593979e915ab2cfd59b956dad5c5
Robert McLay <mclay@zaniahgroup.com>
2021-07-31 09:48:47 -0500

    Remove here statements from src/libxalt/walkProcessTree.c

--- src/libxalt/walkProcessTree.c
+++ src/libxalt/walkProcessTree.c
@@ -13,9 +13,7 @@ void walkProcessTree(pid_t ppid, processTree_t** ptA)
 
   process_t proc;
 
-  HERE;
   init_proc(&proc);
-  HERE;
 
   while(1)
     {
@@ -23,31 +21,23 @@ void walkProcessTree(pid_t ppid, processTree_t** ptA)
       if (proc.m_parent < 2)
         break;
 
-  HERE;
       entry = (processTree_t *) malloc(sizeof(processTree_t));
       if (! entry )
         exit(-1);
 
-  HERE;
       utstring_new(entry->m_path);
       utstring_bincpy(entry->m_path, utstring_body(proc.m_exe), utstring_len(proc.m_exe));
 
-  HERE;
       utstring_new(entry->m_name);
       utstring_bincpy(entry->m_name, utstring_body(proc.m_name), utstring_len(proc.m_name));
       
-  HERE;
       utarray_new(entry->m_cmdlineA, &ut_str_icd);
       proc_cmdline(&proc, &entry->m_cmdlineA);
       entry->m_pid = proc.m_pid;
 
-  HERE;
       DL_APPEND(*ptA, entry);
       my_pid = proc.m_parent;
-  HERE;
     }
-
-  HERE;
   free_proc(&proc);
 }
 
