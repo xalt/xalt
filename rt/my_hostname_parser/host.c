61bdcab809281b95d2b5e3a8e3ca2f4f908ec3c1
Robert McLay <mclay@zaniahgroup.com>
2023-02-12 19:16:13 -0700

    testing my_hostname_parser

--- rt/my_hostname_parser/host.c
+++ rt/my_hostname_parser/host.c
@@ -1,8 +1,10 @@
 #include <xalt_obfuscate.h>
+#include <stdio.h>
 int return_keep();
 
 int my_hostname_parser(const char *hostname)
 {
+  printf(stderr,"In my_hostname_parser\n");
   return return_keep();
 }
 
