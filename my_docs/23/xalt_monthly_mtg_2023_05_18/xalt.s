9194acc7b25121a64c042fc28fb5c1f0e7759661
Robert McLay <mclay@zaniahgroup.com>
2023-05-15 11:50:16 -0500

    Prez 5/18

---
+++ my_docs/23/xalt_monthly_mtg_2023_05_18/xalt.s
@@ -0,0 +1,74 @@
+/* This generated assembly code (/tmp/mclay_000c0d70-2716-4006-94ae-d73f2edbf4bd_lqHIOi/xalt.s) is free and unencumbered software released into the public domain */
+/* Anyone is free to copy, modify, publish, use, compile, sell, or */
+/* distribute this software, either in source code form or as a compiled */
+/* binary, for any purpose, commercial or non-commercial, and by any */
+/* means. */
+
+/* In jurisdictions that recognize copyright laws, the author or authors */
+/* of this software dedicate any and all copyright interest in the */
+/* software to the public domain. We make this dedication for the benefit */
+/* of the public at large and to the detriment of our heirs and */
+/* successors. We intend this dedication to be an overt act of */
+/* relinquishment in perpetuity of all present and future rights to this */
+/* software under copyright law. */
+
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR */
+/* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, */
+/* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR */
+/* OTHER DEALINGS IN THE SOFTWARE. */
+
+	.file    "xalt.s"
+	.section .note.GNU-stack,"",@progbits
+	.section .xalt
+	.asciz "XALT_Link_Info"
+
+	.byte 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	.asciz "<XALT_Version>%%2.10.45%%"
+	.asciz "<Build_host>%%jedrik%%"
+	.asciz "<Build_Syshost>%%jedrik%%"
+	.asciz "<Build_compiler>%%gcc%%"
+	.asciz "<Build_compilerPath>%%/usr/bin/x86_64-linux-gnu-gcc-11%%"
+	.asciz "<Build_OS>%%Linux_%_%_5.15.0-69-generic%%"
+	.asciz "<Build_User>%%mclay%%"
+	.asciz "<Build_UUID>%%000c0d70-2716-4006-94ae-d73f2edbf4bd%%"
+	.asciz "<Build_Year>%%2023%%"
+	.asciz "<Build_date>%%Fri_%_%_Apr_%_%_07_%_%_05:45:40_%_%_2023%%"
+	.asciz "<Build_LOADEDMODULES>%%unix/unix:local/local:mkl/mkl:noweb/2.11b:StdEnv:ddt/ddt:fdepend/fdepend:settarg:gcc/11.2.0:mpich/4.0.2:boost/1.76.0%%"
+	.asciz "<Build_LMFILES>%%/opt/apps/modulefiles/Core/unix/unix.lua:/opt/apps/modulefiles/Core/local/local.lua:/opt/apps/modulefiles/Core/mkl/mkl.lua:/opt/apps/modulefiles/Core/noweb/2.11b.lua:/opt/apps/modulefiles/Core/StdEnv.lua:/opt/apps/modulefiles/Core/ddt/ddt.lua:/opt/apps/modulefiles/Core/fdepend/fdepend.lua:/opt/apps/lmod/lmod/modulefiles/Core/settarg.lua:/opt/apps/modulefiles/Core/gcc/11.2.0.lua:/opt/apps/modulefiles/Compiler/gcc/11/mpich/4.0.2.lua:/opt/apps/modulefiles/Compiler/gcc/11/boost/1.76.0.lua%%"
+	.asciz "<Build_CWD>%%/home/mclay/w/xalt/main/rt/end2end/t1/x86_64_06_avx2_dbg_gcc-11.2.0_mpich-4.0.2-2022_12_14_20_47_28-Linux-x86_64-end2end%%"
+	.asciz "<Build_Epoch>%%1680864340.3491%%"
+	.byte 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	.asciz "XALT_Link_Info_End"
+
+/* This block is readable by a vendor note. */
+
+	.section .note.GNU-stack,"",@progbits
+	.section .note.xalt.info,"a"
+	.p2align 2
+	.long 1f - 0f /* name size (not including padding */
+	.long 3f - 2f /* desc size (not including padding */
+	.long 0x746c6158
+0:	.asciz "XALT"
+1:	.p2align 2
+2:	.byte 0x02
+	.asciz "XALT_Link_Info"
+	.asciz "<XALT_Version>%%2.10.45%%"
+	.asciz "<Build_host>%%jedrik%%"
+	.asciz "<Build_Syshost>%%jedrik%%"
+	.asciz "<Build_compiler>%%gcc%%"
+	.asciz "<Build_compilerPath>%%/usr/bin/x86_64-linux-gnu-gcc-11%%"
+	.asciz "<Build_OS>%%Linux_%_%_5.15.0-69-generic%%"
+	.asciz "<Build_User>%%mclay%%"
+	.asciz "<Build_UUID>%%000c0d70-2716-4006-94ae-d73f2edbf4bd%%"
+	.asciz "<Build_Year>%%2023%%"
+	.asciz "<Build_date>%%Fri_%_%_Apr_%_%_07_%_%_05:45:40_%_%_2023%%"
+	.asciz "<Build_LOADEDMODULES>%%unix/unix:local/local:mkl/mkl:noweb/2.11b:StdEnv:ddt/ddt:fdepend/fdepend:settarg:gcc/11.2.0:mpich/4.0.2:boost/1.76.0%%"
+	.asciz "<Build_LMFILES>%%/opt/apps/modulefiles/Core/unix/unix.lua:/opt/apps/modulefiles/Core/local/local.lua:/opt/apps/modulefiles/Core/mkl/mkl.lua:/opt/apps/modulefiles/Core/noweb/2.11b.lua:/opt/apps/modulefiles/Core/StdEnv.lua:/opt/apps/modulefiles/Core/ddt/ddt.lua:/opt/apps/modulefiles/Core/fdepend/fdepend.lua:/opt/apps/lmod/lmod/modulefiles/Core/settarg.lua:/opt/apps/modulefiles/Core/gcc/11.2.0.lua:/opt/apps/modulefiles/Compiler/gcc/11/mpich/4.0.2.lua:/opt/apps/modulefiles/Compiler/gcc/11/boost/1.76.0.lua%%"
+	.asciz "<Build_CWD>%%/home/mclay/w/xalt/main/rt/end2end/t1/x86_64_06_avx2_dbg_gcc-11.2.0_mpich-4.0.2-2022_12_14_20_47_28-Linux-x86_64-end2end%%"
+	.asciz "<Build_Epoch>%%1680864340.3491%%"
+	.byte 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	.asciz "XALT_Link_Info_End"
+3:	.p2align 2
